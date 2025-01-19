40623f464dbca462405777ae2f4ccb0385e45cd8
Tanmayi Ghadge <tanmayighadge@gmail.com>
2025-01-18 19:44:07 -0700

    two-sum-2

---
+++ two pointers/two-sum-2-input-array-is-sorted.java
@@ -0,0 +1,25 @@
+//two pointer/optimal solution
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+
+        int l = 0;
+        int r = numbers.length - 1;
+
+        while(l < r)
+        {
+            if(numbers[l] + numbers[r] < target)
+            {
+                l++;
+            }
+            else if(numbers[l] + numbers[r] > target)
+            {
+                r--;
+            }
+            else if(numbers[l] + numbers[r] == target)
+            {
+                return new int[] {l+1, r+1};
+            }
+        }
+        return new int[] {0,0};
+    }
+}
